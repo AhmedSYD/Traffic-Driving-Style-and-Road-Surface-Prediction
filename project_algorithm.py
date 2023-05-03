6303726b806df8a0edccb4c01c4beb08f0a232ed
AhmedSYD <ahmedsys1995@yahoo.com>
2023-05-02 23:00:49 -0600

    finalize the model

--- project_algorithm.py
+++ project_algorithm.py
@@ -1,6 +1,7 @@
 import pandas as pd
 from sklearn.model_selection import train_test_split
 from sklearn.ensemble import RandomForestClassifier
+from sklearn import svm
 from sklearn.metrics import confusion_matrix
 from sklearn.metrics import precision_recall_fscore_support
 from sklearn.metrics import roc_curve,roc_auc_score
@@ -15,6 +16,10 @@ import os
 import glob
 import math
 import seaborn as sn
+import warnings 
+
+
+warnings.filterwarnings('ignore')
 
 def draw_histogram(df,attrName,binsno=3, filename=None,visualize=False):
 
@@ -28,19 +33,32 @@ def draw_histogram(df,attrName,binsno=3, filename=None,visualize=False):
         plt.show()
     plt.cla()
     plt.close()
-def train_model(x_train, y_train,x_test,outTargetName):
-    # Create the model with 100 trees
-    model = RandomForestClassifier(n_estimators=100,
-                                   bootstrap = True,
-                                   max_features = 'sqrt')
-    # Fit on training data
-    # print("y_train[columns[0]]=",y_train[columns[0]])
-    model.fit(x_train, y_train[outTargetName])
+def train_model(x_train, y_train,x_test,outTargetName, model_name="RandomForest"):
+    if(model_name=="SVM"):
+        # Create a svm Classifier
+        clf = svm.SVC(kernel='linear', probability=True)  # Linear Kernel
 
-    # Actual class predictions
-    rf_predictions = model.predict(x_test)
-    rf_probs = model.predict_proba(x_test)
-    return rf_predictions,rf_probs
+        # Train the model using the training sets
+        clf.fit(x_train, y_train[outTargetName])
+
+        # Predict the response for test dataset
+        y_pred = clf.predict(x_test)
+        y_pred_prob = clf.predict_proba(x_test)
+        return y_pred,y_pred_prob
+
+    else:
+        # Create the model with 100 trees
+        model = RandomForestClassifier(n_estimators=100,
+                                       bootstrap = True,
+                                       max_features = 'sqrt')
+        # Fit on training data
+        # print("y_train[columns[0]]=",y_train[columns[0]])
+        model.fit(x_train, y_train[outTargetName])
+
+        # Actual class predictions
+        rf_predictions = model.predict(x_test)
+        rf_probs = model.predict_proba(x_test)
+        return rf_predictions,rf_probs
 
 def validate_model(y_test,rf_predictions,rf_probs,outTargetName,num_classes=3):
     # Calculate roc auc
@@ -73,7 +91,7 @@ def main():
     # print(Y.columns)
     x_train, x_test, y_train, y_test = train_test_split(X, Y, train_size=0.7, random_state=90)
     print("col[2]=",columns[2])
-    rf_predictions,rf_probs=train_model(x_train, y_train, x_test, columns[2])
+    rf_predictions,rf_probs=train_model(x_train, y_train, x_test, columns[2],model_name="SVM")
     validate_model(y_test, rf_predictions, rf_probs, columns[2],num_classes=2)
 
 
